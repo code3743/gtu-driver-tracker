f1b2dee0b359a9f062bce18af0f8c4e7d6cb6395
code3743 <jota.lopez@correounivalle.edu.co>
2025-06-16 10:39:56 -0500

    Add initial project structure with Maven wrapper and Spring Boot setup

---
+++ src/main/java/com/gtu/driver_tracker/DriverTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gtu.driver_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverTrackerApplication.class, args);
+	}
+
+}
