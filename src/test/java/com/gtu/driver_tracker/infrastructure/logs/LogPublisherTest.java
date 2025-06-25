ea5d855e2891d1590eb3f82a685b39c0e59cbd2d
SantiReRo19 <santiago.reyes.rodriguez@correounivalle.edu.co>
2025-06-25 12:23:57 -0500

    delete unnecesary import

--- src/test/java/com/gtu/driver_tracker/infrastructure/logs/LogPublisherTest.java
+++ src/test/java/com/gtu/driver_tracker/infrastructure/logs/LogPublisherTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
